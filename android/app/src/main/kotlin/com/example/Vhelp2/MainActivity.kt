0dbc00b1f89dd78d0039df0729bbc2d3a79263a2
Anderson Ribeiro <anderson_ribeiro.a@hotmail.com>
2020-07-23 04:13:53 -0300

    Criação do Projeto, além de implementar alguns plugins que será utilizado na aplicação como Mobx, Flutter_Mobx, Firebase_auth e etc...; Feito a configuração do Firebase e criado o Projeto.

---
+++ android/app/src/main/kotlin/com/example/Vhelp2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.Vhelp2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/app_module.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Vhelp2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Vhelp 2.0',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Container(),
+    );
+  }
+}
